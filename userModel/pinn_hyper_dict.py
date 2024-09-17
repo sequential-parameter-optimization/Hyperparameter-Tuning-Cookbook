9aea98a43952b9c3a3dfe785dfbfada8e1991132
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2024-09-17 09:29:27 +0200

    pinns begin

---
+++ userModel/pinn_hyper_dict.py
@@ -0,0 +1,44 @@
+import json
+from spotpython.data import base
+import pathlib
+
+
+class PINNHyperDict(base.FileConfig):
+    """User specified hyperparameter dictionary.
+
+    This class extends the FileConfig class to provide a dictionary for storing hyperparameters.
+
+    Attributes:
+        filename (str):
+            The name of the file where the hyperparameters are stored.
+    """
+
+    def __init__(
+        self,
+        filename: str = "pinn_hyper_dict.json",
+        directory: None = None,
+    ) -> None:
+        super().__init__(filename=filename, directory=directory)
+        self.filename = filename
+        self.directory = directory
+        self.hyper_dict = self.load()
+
+    @property
+    def path(self):
+        if self.directory:
+            return pathlib.Path(self.directory).joinpath(self.filename)
+        return pathlib.Path(__file__).parent.joinpath(self.filename)
+
+    def load(self) -> dict:
+        """Load the hyperparameters from the file.
+
+        Returns:
+            dict: A dictionary containing the hyperparameters.
+
+        Examples:
+            # Assume the user specified file `my_hyper_dict.json` is in the `./hyperdict/` directory.
+            >>> user_lhd = MyHyperDict(filename='my_hyper_dict.json', directory='./hyperdict/')
+        """
+        with open(self.path, "r") as f:
+            d = json.load(f)
+        return d
